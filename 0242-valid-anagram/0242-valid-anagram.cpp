a0c9dd49b772591e299998f42533c229852409a1
Saurabh Saroj <145561257+saurabh2728@users.noreply.github.com>
2026-07-10 11:00:33 +0530

    Time: 0 ms (100%), Space: 9.7 MB (49.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,55 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         int k = pat.size();
+//         int m = txt.size();
+
+//         vector<int>p(26,0);
+//         vector<int>t(26,0);
+
+//         for(auto c:pat){
+//             p[c-'a']++;
+
+//         }
+//         int i=0;j = 0;ans=o;
+//         while(j<m){
+//             t[txt[j]-'a']++;
+//             if(j-i+1<k)j++;
+//             else if(j-i+1==k){
+//                 if(p==t){
+//                     ans++;
+//                 }
+//                 t[txt[i]-'a']--;
+//                 i++;
+//                 j++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.size() != t.size())
+            return false;
+        unordered_map<char, int> mp;
+        for(int i = 0; i < s.size(); i++)
+        {
+            mp[s[i]]++;
+        }
+        for(int i = 0; i < t.size(); i++)
+        {
+            mp[t[i]]--;
+        }
+        for(auto x : mp)
+        {
+            if(x.second != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
