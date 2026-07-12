dff0c9abd2d75e5a7ae7327d491244d30d1103c4
Saurabh Saroj <145561257+saurabh2728@users.noreply.github.com>
2026-07-12 21:30:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.22%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,15 +1,3 @@
-
-// using recursion
-// class Solution {
-// public:
-//     bool isPowerOfTwo(int n) {
-//         if(n==1) return 1;
-//         if(n%2!=0 || n<=0) return 0;
-//         return isPowerOfTwo(n/2);
-        
-//     }
-// };
-
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
