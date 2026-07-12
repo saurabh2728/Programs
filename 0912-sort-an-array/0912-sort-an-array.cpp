fd99187f79a636e0a15d16d231f51702a1b23b40
Saurabh Saroj <145561257+saurabh2728@users.noreply.github.com>
2026-07-12 22:11:58 +0530

    Time: 19 ms (89.92%), Space: 70.9 MB (93.6%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,3 +1,7 @@
-void stlSort(vector<int>& nums) {
-	sort(nums.begin(), nums.end());
-}
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
