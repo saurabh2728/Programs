28a9b91ea1c7cd17302316705d80e01dc2162510
Saurabh Saroj <145561257+saurabh2728@users.noreply.github.com>
2026-07-14 15:32:35 +0530

    Time: 2 ms (5.71%), Space: 13.3 MB (5.11%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,18 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+// vector<int> ans;
+//     void trav(TreeNode* root,vector<int>&ans) {
+//         if(root==NULL) return;
+//         trav(root->left,ans);
+//         trav(root->right,ans);
+//         ans.push_back(root->val);
+//     }
+//     vector<int> postorderTraversal(TreeNode* root){
+//         vector<int>ans;
+//         trav(root,ans);
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-vector<int> ans;
-    void trav(TreeNode* root,vector<int>&ans) {
-        if(root==NULL) return;
-        trav(root->left,ans);
-        trav(root->right,ans);
+    vector<int> ans;
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (root == NULL)
+            return ans;
+
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);  
         ans.push_back(root->val);
-    }
-    vector<int> postorderTraversal(TreeNode* root){
-        vector<int>ans;
-        trav(root,ans);
-        return ans;
+
+        return ans;                        
     }
 };
