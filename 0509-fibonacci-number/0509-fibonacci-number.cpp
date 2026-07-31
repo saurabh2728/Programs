d0367fb790d4a39c3e192050e1b2a8e62c6692ae
Saurabh Saroj <145561257+saurabh2728@users.noreply.github.com>
2026-07-31 12:21:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (89.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,49 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if (n <= 1) return n;
+//         int a = 0, b = 1;
+
+//         for (int i = 2; i <= n; i++) {
+//             int c = a + b;
+//             a = b;
+//             b = c;
+//         }
+//         return b;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int solve(int n, vector<int>& memo) {
+//         if (n <= 1) return n;
+
+//         if (memo[n] != -1) 
+//             return memo[n];
+
+//         memo[n] = solve(n - 1, memo) + solve(n - 2, memo);
+//         return memo[n];
+//     }
+
+//     int fib(int n) {
+//         vector<int> memo(n + 1, -1);
+//         return solve(n, memo);
+//     }
+// };   
 class Solution {
 public:
     int fib(int n) {
-        if (n <= 1) return n;
-        int a = 0, b = 1;
+        int dp[n + 1];
+
+        if (n == 0 || n == 1) return n;
+
+        dp[0] = 0;
+        dp[1] = 1;
 
         for (int i = 2; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return b;
+
+        return dp[n];
     }
-};
+};     
